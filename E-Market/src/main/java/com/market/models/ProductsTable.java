eee64c93dc477bba0283c0eb32137ba0f947056b
Igor Dzyombak <dzyombak.igor@gmail.com>
2016-04-04 22:50:48 +0300

    Iteration 4, commit 2

---
+++ E-Market/src/main/java/com/market/models/ProductsTable.java
@@ -0,0 +1,49 @@
+package com.market.models;
+
+public class ProductsTable {
+    int id;
+    String model;
+    int year;
+    int mileage;
+    int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
