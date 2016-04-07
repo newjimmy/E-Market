286f13d3c9576d26c1a51d88ce66d2b5de9c8ffd
Igor Dzyombak <dzyombak.igor@gmail.com>
2016-04-07 16:03:50 +0300

    Iteration 6, commit  4 - done. Model and Repository was added.

---
+++ E-Market/src/main/java/com/market/models/Category.java
@@ -0,0 +1,67 @@
+package com.market.models;
+
+public class Category {
+    private int category_id;
+    private  String body_type;
+    private String colour;
+    private int engine_capacity;
+    private int engine_volume;
+    private int max_speed;
+
+    public Category(int category_id, String body_type, String colour, int engine_capacity, int engine_volume, int max_speed) {
+        this.category_id = category_id;
+        this.body_type = body_type;
+        this.colour = colour;
+        this.engine_capacity = engine_capacity;
+        this.engine_volume = engine_volume;
+        this.max_speed = max_speed;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getBody_type() {
+        return body_type;
+    }
+
+    public void setBody_type(String body_type) {
+        this.body_type = body_type;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public int getEngine_capacity() {
+        return engine_capacity;
+    }
+
+    public void setEngine_capacity(int engine_capacity) {
+        this.engine_capacity = engine_capacity;
+    }
+
+    public int getEngine_volume() {
+        return engine_volume;
+    }
+
+    public void setEngine_volume(int engine_volume) {
+        this.engine_volume = engine_volume;
+    }
+
+    public int getMax_speed() {
+        return max_speed;
+    }
+
+    public void setMax_speed(int max_speed) {
+        this.max_speed = max_speed;
+    }
+}
---
+++ E-Market/src/main/java/com/market/repository/CategoriesRepository.java
@@ -0,0 +1,33 @@
+package com.market.repository;
+
+import com.market.models.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class CategoriesRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setJdbcTemplate(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    private final static String QUERY_FETCH_CATEGORIES = "select category_id, body_type, colour, engine_capacity, engine_volume, max_speed from categories";
+
+    public List<Category> getAllCategories() {
+        return jdbcTemplate.query(QUERY_FETCH_CATEGORIES, (resultSet, i) -> {
+            return new Category(
+                    resultSet.getInt("category_id"),
+                    resultSet.getString("body_type"),
+                    resultSet.getString("colour"),
+                    resultSet.getInt("engine_capacity"),
+                    resultSet.getInt("engine_volume"),
+                    resultSet.getInt("max_speed")
+            );
+        });
+    }
+}
